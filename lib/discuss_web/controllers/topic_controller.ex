31a542f717198458a5ad5a9a60563435044a450a
Zsolt <zsolt.burzas@infinitaslearning.com>
2024-02-24 14:25:06 +0100

    feat: delete topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -48,4 +48,12 @@ defmodule DiscussWeb.TopicController do
       {:error, changeset} -> render(conn, :edit, changeset: changeset, topic: old_topic)
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic deleted")
+    |> redirect(to: ~p"/")
+  end
 end
