510743cc607742b2d87d3e2d13509a49f2a23de4
Zsolt <zsolt.burzas@infinitaslearning.com>
2024-02-25 18:22:01 +0100

    feat: check for owner

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -5,6 +5,7 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Repo
 
   plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
@@ -19,7 +20,9 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns.user
+      |> Ecto.build_assoc(:topics)
+      |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _post} ->
@@ -57,5 +60,18 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic deleted")
     |> redirect(to: ~p"/")
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if (Repo.get(Topic, topic_id).user_id == conn.assigns.user.id) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit!")
+      |> redirect(to: ~p"/")
+      |> halt()
+    end
   end
 end
